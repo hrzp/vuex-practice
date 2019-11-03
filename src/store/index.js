a83827964f5936e341a772a683bc0afa92fa78dc
hamid reza zare pour <hamidreza.num1@gmail.com>
2019-11-03 14:23:11 +0330

    Initial commit

--- src/store/index.js
+++ src/store/index.js
@@ -4,7 +4,10 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
+  state: {
+    message: "Hello from Vuex",
+    count: 0
+  },
   mutations: {},
   actions: {},
   modules: {}
